17b96d75a53272dc3ac22cb6c8c8003c4852f48b
Tom McFarlin <tom.mcfarlin@webdevstudios.com>
2020-12-07 14:41:22 -0500

    Add proper version to this plugin since its a full rewrite

---
+++ plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Category Sticky Post
+ *
+ * Mark a post to be displayed at the top of the category archive page.
+ *
+ * php version 7.3.21
+ *
+ * @category  WordPress_Plugin
+ * @package   CategoryStickyPost
+ * @author    Tom McFarlin <tom@tommcfarlin.com>
+ * @copyright 2013 - 2020 Tom McFarlin
+ * @license   GPL 3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>
+ * @link      https://tommcfarlin.com/category-sticky-post/
+ *
+ * @wordpress-plugin
+ * Plugin Name:  Category Sticky Post
+ * Plugin URI:   https://tommcfarlin.com/category-sticky-post/
+ * Description:  Mark a post to be displayed at the top of the category archive page.
+ * Requires PHP: 7.3.21
+ * Version:      3.0.0
+ * Author:       Tom McFarlin
+ * Author URI:   https://tommcfarlin.com
+ * Text Domain:  category-sticky-post
+ * License:      GPL-3.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+// If this file is called directly, abort.
+if (! defined('WPINC') ) {
+    die;
+}
