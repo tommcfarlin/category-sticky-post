4848806bf49c6412e26398808ecfa0d32aa9d103
Tom McFarlin <tom.mcfarlin@webdevstudios.com>
2020-12-08 15:46:17 -0500

    Add categories select element

--- src/WordPress/Admin/Views/Categories.php
+++ src/WordPress/Admin/Views/Categories.php
@@ -3,5 +3,14 @@
 ?>
 <!-- TODO change styles -->
 <div class="category-sticky-post" style="margin:0 0 -1em 0 !important;padding:0em;">
-	<p style="margin:0 !important;padding:1em;">This is where the categories will go.</p>
+	<p style="margin:0 !important;padding:1em;">
+		<select style="width:90%;">
+			<option value="0">Select a category...</option>
+			<?php foreach ( get_categories() as $category ) : ?>
+				<option value="<?php $category->term_id; ?>">
+					<?php echo ucwords( $category->category_nicename ); // phpcs:ignore ?>
+				</option>
+			<?php endforeach; ?>
+		</select>
+	</p>
 </div><!-- .wrap -->
