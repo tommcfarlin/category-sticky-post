d0c517a0a80cf9830eb0a917e54db43c1ee056d6
Tom McFarlin <tommcfarlin@Skyhopper6.local>
2020-12-09 13:34:07 -0500

    Add support for reordering category archive page with a sticky post

--- src/Utilities/Registry.php
+++ src/Utilities/Registry.php
@@ -4,12 +4,14 @@ namespace TomMcFarlin\CategoryStickyPost\Utilities;
 
 use TomMcFarlin\CategoryStickyPost\Subscriber\MetaBoxSubscriber;
 use TomMcFarlin\CategoryStickyPost\Subscriber\SavePostSubscriber;
+use TomMcFarlin\CategoryStickyPost\Subscriber\CategoryPageSubscriber;
 
 class Registry {
 
 	private $subscribers = [
 		MetaBoxSubscriber::class,
 		SavePostSubscriber::class,
+		CategoryPageSubscriber::class,
 	];
 
 	public function run() {
