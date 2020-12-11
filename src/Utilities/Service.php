451f2035e75618660138c0679fcb12d55ac03848
Tom McFarlin <tom.mcfarlin@webdevstudios.com>
2020-12-11 11:51:56 -0500

    Update method signatures

--- src/Utilities/Service.php
+++ src/Utilities/Service.php
@@ -4,7 +4,10 @@ namespace TomMcFarlin\CategoryStickyPost\Utilities;
 
 use WP_Query;
 
-class Service {
+abstract class Service {
+
+	public abstract function add( string $hook );
+
 	protected function is_category_page( WP_Query $query ) {
 		return (
 			$query->is_main_query() && is_archive() && 0 === get_query_var( 'paged' ) && '' !== get_query_var( 'cat' )
